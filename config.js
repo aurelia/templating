6d3a3b966f7dbf94a88de5cf440cad5894962f3a
Rob Eisenberg <rob@bluespire.com>
2014-12-11 20:41:35 -0500

    fix(package): update dependencies to latest versions

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ System.config({
     "aurelia-binding": "github:aurelia/binding@0.1.0",
     "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.1.0",
     "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.0",
-    "aurelia-loader": "github:aurelia/loader@0.1.0",
+    "aurelia-loader": "github:aurelia/loader@0.1.1",
     "aurelia-logging": "github:aurelia/logging@0.1.0",
     "aurelia-metadata": "github:aurelia/metadata@0.1.0",
     "aurelia-path": "github:aurelia/path@0.1.0",
@@ -24,11 +24,11 @@ System.config({
       "aurelia-metadata": "github:aurelia/metadata@0.1.0",
       "es6-shim": "github:paulmillr/es6-shim@0.21.1"
     },
-    "github:aurelia/loader@0.1.0": {
+    "github:aurelia/loader@0.1.1": {
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.0",
       "aurelia-path": "github:aurelia/path@0.1.0",
       "es6-shim": "github:paulmillr/es6-shim@0.21.1",
-      "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.1"
+      "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.2"
     }
   }
 });
