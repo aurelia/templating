4a3489f983b75f194f38b0142fd4fe2ee3680184
Rob Eisenberg <rob@bluespire.com>
2015-02-28 00:30:32 -0500

    fix(package): update dependencies

--- config.js
+++ config.js
@@ -9,28 +9,28 @@ System.config({
 
 System.config({
   "map": {
-    "aurelia-binding": "github:aurelia/binding@0.3.5",
-    "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.3",
-    "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.2",
-    "aurelia-loader": "github:aurelia/loader@0.3.3",
-    "aurelia-logging": "github:aurelia/logging@0.2.3",
-    "aurelia-metadata": "github:aurelia/metadata@0.3.1",
-    "aurelia-path": "github:aurelia/path@0.4.1",
-    "aurelia-task-queue": "github:aurelia/task-queue@0.2.3",
+    "aurelia-binding": "github:aurelia/binding@0.3.6",
+    "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.4",
+    "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.3",
+    "aurelia-loader": "github:aurelia/loader@0.3.4",
+    "aurelia-logging": "github:aurelia/logging@0.2.4",
+    "aurelia-metadata": "github:aurelia/metadata@0.3.2",
+    "aurelia-path": "github:aurelia/path@0.4.4",
+    "aurelia-task-queue": "github:aurelia/task-queue@0.2.4",
     "core-js": "npm:core-js@0.4.10",
-    "github:aurelia/binding@0.3.5": {
-      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.3",
-      "aurelia-metadata": "github:aurelia/metadata@0.3.1",
-      "aurelia-task-queue": "github:aurelia/task-queue@0.2.3"
+    "github:aurelia/binding@0.3.6": {
+      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.4",
+      "aurelia-metadata": "github:aurelia/metadata@0.3.2",
+      "aurelia-task-queue": "github:aurelia/task-queue@0.2.4"
     },
-    "github:aurelia/dependency-injection@0.4.3": {
-      "aurelia-metadata": "github:aurelia/metadata@0.3.1",
+    "github:aurelia/dependency-injection@0.4.4": {
+      "aurelia-metadata": "github:aurelia/metadata@0.3.2",
       "core-js": "npm:core-js@0.4.10"
     },
-    "github:aurelia/loader@0.3.3": {
-      "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.2",
+    "github:aurelia/loader@0.3.4": {
+      "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.3",
       "core-js": "npm:core-js@0.4.10",
-      "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.4"
+      "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.5"
     },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.0"
