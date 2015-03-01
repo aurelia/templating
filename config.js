9ef08cfd9596600b161ed7c686f53b15cfdaa5de
Rob Eisenberg <rob@bluespire.com>
2015-02-28 19:58:25 -0500

    fix(package): change jspm directories

--- config.js
+++ config.js
@@ -9,25 +9,25 @@ System.config({
 
 System.config({
   "map": {
-    "aurelia-binding": "github:aurelia/binding@0.3.6",
-    "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.4",
+    "aurelia-binding": "github:aurelia/binding@0.3.7",
+    "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.5",
     "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.3",
-    "aurelia-loader": "github:aurelia/loader@0.3.4",
-    "aurelia-logging": "github:aurelia/logging@0.2.4",
-    "aurelia-metadata": "github:aurelia/metadata@0.3.2",
-    "aurelia-path": "github:aurelia/path@0.4.4",
-    "aurelia-task-queue": "github:aurelia/task-queue@0.2.4",
+    "aurelia-loader": "github:aurelia/loader@0.3.5",
+    "aurelia-logging": "github:aurelia/logging@0.2.5",
+    "aurelia-metadata": "github:aurelia/metadata@0.3.3",
+    "aurelia-path": "github:aurelia/path@0.4.5",
+    "aurelia-task-queue": "github:aurelia/task-queue@0.2.5",
     "core-js": "npm:core-js@0.4.10",
-    "github:aurelia/binding@0.3.6": {
-      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.4",
-      "aurelia-metadata": "github:aurelia/metadata@0.3.2",
-      "aurelia-task-queue": "github:aurelia/task-queue@0.2.4"
+    "github:aurelia/binding@0.3.7": {
+      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.4.5",
+      "aurelia-metadata": "github:aurelia/metadata@0.3.3",
+      "aurelia-task-queue": "github:aurelia/task-queue@0.2.5"
     },
-    "github:aurelia/dependency-injection@0.4.4": {
-      "aurelia-metadata": "github:aurelia/metadata@0.3.2",
+    "github:aurelia/dependency-injection@0.4.5": {
+      "aurelia-metadata": "github:aurelia/metadata@0.3.3",
       "core-js": "npm:core-js@0.4.10"
     },
-    "github:aurelia/loader@0.3.4": {
+    "github:aurelia/loader@0.3.5": {
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.3",
       "core-js": "npm:core-js@0.4.10",
       "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.5"
