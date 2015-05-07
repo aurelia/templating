7ae88b6fccf92cc057a7964c952d3ff925301725
Martin Gustafsson <gusten86@gmail.com>
2015-05-07 23:05:00 +0200

    fix(view-slot): add fallback for missing nextElementSibling
    Closes aurelia/animator-css#8

--- src/util.js
+++ src/util.js
@@ -7,3 +7,10 @@ function addHyphenAndLower(char){
 export function hyphenate(name){
   return (name.charAt(0).toLowerCase() + name.slice(1)).replace(capitalMatcher, addHyphenAndLower);
 }
+
+export function nextElementSibling(element) {
+  if (element.nextElementSibling){ return element.nextElementSibling; }
+  do { element = element.nextSibling }
+  while (element && element.nodeType !== 1);
+  return element;
+}
