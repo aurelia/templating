959b71e9169670d3c964b91f0deabbe4ff610611
MauriceRoseboom <maurice.roseboom@gmail.com>
2017-01-20 19:04:41 +0100

    resolve merge conflicts

--- src/composition-engine.js
+++ src/composition-engine.js
@@ -69,37 +69,6 @@ function tryActivateViewModel(context) {
   return context.viewModel.activate(context.model) || Promise.resolve();
 }
 
-class SwapStrategies {
-  // animate the next view in before removing the current view;
-  before(viewSlot, previousView, callback) {
-    let promise = Promise.resolve(callback());
-
-    if (previousView !== undefined) {
-      return promise.then(() => viewSlot.remove(previousView, true));
-    }
-
-    return promise;
-  }
-
-  // animate the next view at the same time the current view is removed
-  with(viewSlot, previousView, callback) {
-    let promise = Promise.resolve(callback());
-
-    if (previousView !== undefined) {
-      return Promise.all([viewSlot.remove(previousView, true), promise]);
-    }
-
-    return promise;
-  }
-
-  // animate the next view in after the current view has been removed
-  after(viewSlot, previousView, callback) {
-    return Promise.resolve(viewSlot.removeAll(true)).then(callback);
-  }
-}
-
-const swapStrategies = new SwapStrategies();
-
 /**
 * Used to dynamically compose components.
 */
