e41dc857df3dabca1dc8056889fb86b209928fc1
MauriceRoseboom <maurice.roseboom@gmail.com>
2017-02-19 18:22:27 +0100

    refactor (swapStrategy): move removing of old views to own function

--- src/swap-strategies.js
+++ src/swap-strategies.js
@@ -1,24 +1,26 @@
+function remove(viewSlot, previous) {
+  return Array.isArray(previous) 
+    ? viewSlot.removeMany(previous, true) 
+    : viewSlot.remove(previous, true);
+}
+
 export const SwapStrategies = {
   // animate the next view in before removing the current view;
-  before(viewSlot, previousViews, callback) {
-    if (previousViews !== undefined) {
-      return callback().then(() => viewSlot.removeMany(previousViews, true));
-    }
-
-    return callback();
+  before(viewSlot, previous, callback) {
+    return (previous === undefined)
+      ? callback()
+      : callback().then(() => remove(viewSlot, previous));
   },
 
   // animate the next view at the same time the current view is removed
   with(viewSlot, previousViews, callback) {
-    if (previousViews !== undefined) {
-      return Promise.all([viewSlot.removeMany(previousViews, true), callback()]);
-    }
-
-    return callback();
+    return (previous === undefined)
+      ? callback()
+      : Promise.all([remove(viewSlot, previous), callback()]);
   },
 
   // animate the next view in after the current view has been removed
-  after(viewSlot, previousView, callback) {
+  after(viewSlot, previous, callback) {
     return Promise.resolve(viewSlot.removeAll(true)).then(callback);
   }
 };
