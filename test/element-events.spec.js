f7e8e324eb21c8c528b15a6ec9096a9206e47684
Sebastien Roch <roch.sebastien@gmail.com>
2019-03-18 10:01:09 +0100

    fix(ElementEvents): ElementEvents.defaultListenerOptions allows to set default options when subscribing to events
    ElementEvents.subscribe will set capture to true be default if not explicitely passed in argument. This is not the standard behavior of the native addEventListener() and confuses developers. This new options let you define the default behavior.
    
    Fixes issue #657

--- test/element-events.spec.js
+++ test/element-events.spec.js
@@ -53,6 +53,48 @@ describe('ElementEvents', () => {
     expect(callCount).toBe(1);
   });
 
+  it("should subscribe and take ElementEvent default listener options into account", () => {
+    let isCapture;
+
+    // we need to track event on a parent of the input, let's take body
+    const bodyElementEvents = new ElementEvents(document.body);
+    ElementEvents.defaultListenerOptions = { capture: false };
+    let eventHandler = bodyElementEvents.subscribe('input', event => {
+      isCapture = event.eventPhase === Event.CAPTURING_PHASE;
+    });
+
+    // input has to be attached for the event to bubble up
+    document.body.appendChild(input);
+    input.dispatchEvent(new CustomEvent('input', {bubbles: true}));
+    expect(isCapture).toBe(false);
+    eventHandler.dispose();
+
+    
+    // set capture back to true and check if it's being used
+    ElementEvents.defaultListenerOptions = { capture: true };
+    eventHandler = bodyElementEvents.subscribe('input', event => {
+      isCapture = event.eventPhase === Event.CAPTURING_PHASE;
+    });
+
+    input.dispatchEvent(new CustomEvent('input', {bubbles: true}));
+    expect(isCapture).toBe(true);
+    eventHandler.dispose();
+  });
+
+  it("should subscribe and ignore ElementEvent default listener options when argument captureOrOptions is passed", () => {
+    let isCapture;
+
+    const bodyElementEvents = new ElementEvents(document.body);
+    ElementEvents.defaultListenerOptions = { capture: false };
+    bodyElementEvents.subscribe('input', event => {
+      isCapture = event.eventPhase === Event.CAPTURING_PHASE;
+    }, true);
+
+    document.body.appendChild(input);
+    input.dispatchEvent(new CustomEvent('input', {bubbles: true}));
+    expect(isCapture).toBe(true);
+  });
+
   it('should dispose single event', () => {
     let value;
     let callCount = 0;
