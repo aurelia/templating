97fde4bf1a5a277fe489b6df5269c185c8a8a199
bigopon.777@gmail.com <bigopon.777@gmail.com>
2020-05-17 20:55:11 +1000

    fix(child-observation): properly unbind/bind between shadowDOM/emulation

--- src/child-observation.js
+++ src/child-observation.js
@@ -38,6 +38,25 @@ export function child(selectorOrConfig: string | Object): any {
   return createChildObserverDecorator(selectorOrConfig, false);
 }
 
+/**
+ * @internal
+ * @typedef MutationObserverBinder
+ * @property {ChildObserverBinder[]} binders
+ * 
+ * @internal
+ * @typedef {MutationObserverBinder & MutationObserver} BindableMutationObserver
+ *
+ * @internal
+ * @typedef HasChildObserver
+ * @property {BindableMutationObserver} __childObserver__
+ * 
+ * @internal
+ * @typedef {Element & HasChildObserver} BindableMutationObserverHost
+ */
+
+/**
+ * Child observer binder factory
+ */
 class ChildObserver {
   constructor(config) {
     this.name = config.name;
@@ -53,6 +72,12 @@ class ChildObserver {
 
 const noMutations = [];
 
+/**
+ * 
+ * @param {Map<ChildObserverBinder, MutationRecord[]>} groupedMutations
+ * @param {ChildObserverBinder} binder
+ * @param {MutationRecord} record
+ */
 function trackMutation(groupedMutations, binder, record) {
   let mutations = groupedMutations.get(binder);
 
@@ -64,9 +89,14 @@ function trackMutation(groupedMutations, binder, record) {
   mutations.push(record);
 }
 
+/**
+ * @param {MutationRecord[]} mutations
+ * @param {BindableMutationObserver} observer
+ */
 function onChildChange(mutations, observer) {
   let binders = observer.binders;
   let bindersLength = binders.length;
+  /**@type {Map<ChildObserverBinder, MutationRecord[]>} */
   let groupedMutations = new Map();
 
   for (let i = 0, ii = mutations.length; i < ii; ++i) {
@@ -79,6 +109,9 @@ function onChildChange(mutations, observer) {
       if (node.nodeType === 1) {
         for (let k = 0; k < bindersLength; ++k) {
           let binder = binders[k];
+          // only track mutation when binder signals so
+          // for @children scenarios where it should only call change handler
+          // after retrieving all value of the children
           if (binder.onRemove(node)) {
             trackMutation(groupedMutations, binder, record);
           }
@@ -91,6 +124,9 @@ function onChildChange(mutations, observer) {
       if (node.nodeType === 1) {
         for (let k = 0; k < bindersLength; ++k) {
           let binder = binders[k];
+          // only track mutation when binder signals so
+          // for @children scenarios where it should only call change handler
+          // after retrieving all value of the children
           if (binder.onAdd(node)) {
             trackMutation(groupedMutations, binder, record);
           }
@@ -99,16 +135,32 @@ function onChildChange(mutations, observer) {
     }
   }
 
-  groupedMutations.forEach((value, key) => {
-    if (key.changeHandler !== null) {
-      key.viewModel[key.changeHandler](value);
+  groupedMutations.forEach((mutationRecords, binder) => {
+    if (binder.isBound && binder.changeHandler !== null) {
+      // invoking with mutation records as new value doesn't make it very useful,
+      // and kind of error prone.
+      // Probably should change it to the value array
+      // though it is a breaking change. Consider changing this
+      binder.viewModel[binder.changeHandler](mutationRecords);
     }
   });
 }
 
 class ChildObserverBinder {
+  /**
+   * @param {string} selector the CSS selector used to filter the content of a host
+   * @param {Element} viewHost the host where this observer belongs to
+   * @param {string} property the property name of the view model where the aggregated result of this observer should assign to
+   * @param {object} viewModel the view model that this observer is associated with
+   * @param {Controller} controller the corresponding Controller of the view model
+   * @param {string} changeHandler the name of the change handler to invoke when the content of the view host change
+   * @param {boolean} all indicates whether it should try to match all children of the view host or not
+   */
   constructor(selector, viewHost, property, viewModel, controller, changeHandler, all) {
     this.selector = selector;
+    /**
+     * @type {Element & HasChildObserver}
+     */
     this.viewHost = viewHost;
     this.property = property;
     this.viewModel = viewModel;
@@ -122,6 +174,8 @@ class ChildObserverBinder {
     } else {
       this.contentView = null;
     }
+    this.source = null;
+    this.isBound = false;
   }
 
   matches(element) {
@@ -152,6 +206,14 @@ class ChildObserverBinder {
   }
 
   bind(source) {
+    if (this.isBound) {
+      if (this.source === source) {
+        return;
+      }
+      this.source = source;
+    }
+    this.isBound = true;
+
     let viewHost = this.viewHost;
     let viewModel = this.viewModel;
     let observer = viewHost.__childObserver__;
@@ -217,6 +279,10 @@ class ChildObserverBinder {
     if (this.matches(element)) {
       let value = element.au && element.au.controller ? element.au.controller.viewModel : element;
 
+      // for @children scenario
+      // when it is selecting all child element
+      // the callback needs to happen AFTER mapping all of the child value
+      // so returning true as a mean to register a value to be added later
       if (this.all) {
         let items = (this.viewModel[this.property] || (this.viewModel[this.property] = []));
         let index = items.indexOf(value);
@@ -228,7 +294,17 @@ class ChildObserverBinder {
         return true;
       }
 
-      return false;
+      // for @child scenario
+      const currentValue = this.viewModel[this.property];
+      if (currentValue === value) {
+        this.viewModel[this.property] = null;
+
+        // when it is a single child observation
+        // it is safe to trigger change handler immediately
+        if (this.isBound && this.changeHandler !== null) {
+          this.viewModel[this.changeHandler](value);
+        }
+      }
     }
 
     return false;
@@ -238,6 +314,10 @@ class ChildObserverBinder {
     if (this.matches(element)) {
       let value = element.au && element.au.controller ? element.au.controller.viewModel : element;
 
+      // for @children scenario
+      // when it is selecting all child element
+      // the callback needs to happen AFTER mapping all of the child value
+      // so returning true as a mean to register a value to be added later
       if (this.all) {
         let items = (this.viewModel[this.property] || (this.viewModel[this.property] = []));
 
@@ -261,9 +341,16 @@ class ChildObserverBinder {
         return true;
       }
 
+      // for @child scenario
+      // in multiple child scenario
+      // it will keep reassigning value to the property
+      // until the last matched element
+      // this is unexpected but not easy to determine otherwise
       this.viewModel[this.property] = value;
 
-      if (this.changeHandler !== null) {
+      // when it is a single child observation
+      // it is safe to trigger change handler immediately
+      if (this.isBound && this.changeHandler !== null) {
         this.viewModel[this.changeHandler](value);
       }
     }
@@ -272,10 +359,28 @@ class ChildObserverBinder {
   }
 
   unbind() {
-    if (this.viewHost.__childObserver__) {
-      this.viewHost.__childObserver__.disconnect();
-      this.viewHost.__childObserver__ = null;
-      this.viewModel[this.property] = null;
+    if (!this.isBound) {
+      return;
+    }
+    this.isBound = false;
+    let childObserver = this.viewHost.__childObserver__;
+    if (childObserver) {
+      let binders = childObserver.binders;
+      if (binders && binders.length) {
+        let idx = binders.indexOf(this);
+        if (idx !== -1) {
+          binders.splice(idx, 1);
+        }
+        if (binders.length === 0) {
+          childObserver.disconnect();
+          this.viewHost.__childObserver__ = null;
+        }
+      }
+      // when using shadowDOM, the bound property is populated during bind
+      // so it's safe to unassign it
+      if (this.usesShadowDOM) {
+        this.viewModel[this.property] = null;
+      }
     }
   }
 }
