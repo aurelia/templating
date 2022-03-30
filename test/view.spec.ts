8837d7ed505bf1aad17330b482ce26835f043187
bigopon <bigopon.777@gmail.com>
2022-03-31 00:43:01 +1100

    refactor(tests): rename .js to .ts

--- test/view.spec.ts
+++ test/view.spec.ts
@@ -1,14 +1,16 @@
-import {View} from '../src/view';
+import { Binding, Scope } from 'aurelia-binding';
+import { Container } from 'aurelia-dependency-injection';
+import {Controller, View, ViewFactory} from '../src/aurelia-templating';
 
 describe('View', () => {
   it('binds and unbinds', () => {
     let view;
-    let bindingContext = {};
-    let overrideContext = {};
-    let container = {};
-    let viewFactory = { resources: { _invokeHook: jasmine.createSpy('_invokeHook') } };
-    let fragment = {};
-    let controllers = [{ bind: jasmine.createSpy('bind'), unbind: jasmine.createSpy('unbind') }];
+    let bindingContext = {} as Scope;
+    let overrideContext = {} as Scope;
+    let container = {} as Container;
+    let viewFactory = { resources: { _invokeHook: jasmine.createSpy('_invokeHook') } } as unknown as ViewFactory;
+    let fragment = {} as DocumentFragment;
+    let controllers = [{ bind: jasmine.createSpy('bind'), unbind: jasmine.createSpy('unbind') }] as unknown as Controller[];
     let bindings = [{
       bind: jasmine.createSpy('bind'),
       unbind: jasmine.createSpy('unbind').and.callFake(function() {
@@ -16,15 +18,15 @@ describe('View', () => {
         expect(view.bindingContext).toBe(bindingContext);
         expect(view.overrideContext).toBe(overrideContext);
       })
-    }];
-    let children = [{ bind: jasmine.createSpy('bind'), unbind: jasmine.createSpy('unbind') }];
+    }] as unknown as Binding[];
+    let children = [{ bind: jasmine.createSpy('bind'), unbind: jasmine.createSpy('unbind') }] as unknown as Binding[];
     let contentSelectors = [];
     view = new View(container, viewFactory, fragment, controllers, bindings, children, contentSelectors);
     view.bind(bindingContext, overrideContext);
     expect(viewFactory.resources._invokeHook).toHaveBeenCalledWith('beforeBind', view);
     expect(controllers[0].bind).toHaveBeenCalledWith(view);
     expect(bindings[0].bind).toHaveBeenCalledWith(view);
-    expect(children[0].bind).toHaveBeenCalledWith(bindingContext, overrideContext, true);
+    expect(children[0].bind as any).toHaveBeenCalledWith(bindingContext, overrideContext, true);
     view.unbind();
     expect(viewFactory.resources._invokeHook).toHaveBeenCalledWith('beforeUnbind', view);
     expect(controllers[0].unbind).toHaveBeenCalled();
