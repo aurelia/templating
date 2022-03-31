69b358ac835ec49992d31ec769a8b2a5a0d0de10
bigopon <bigopon.777@gmail.com>
2022-03-31 15:51:49 +1100

    chore(lint): upgrade eslint, tweak rules, fix linting issues

--- test/behaviors/simple-attribute.ts
+++ test/behaviors/simple-attribute.ts
@@ -2,5 +2,6 @@ import {customAttribute} from '../../src/decorators';
 
 @customAttribute('simple-attribute')
 export class SimpleAttribute {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   valueChanged(newValue, oldValue){ }
 }
--- test/behaviors/simple-element.ts
+++ test/behaviors/simple-element.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {bindable} from '../../src/decorators';
 
 export class SimpleElement {
